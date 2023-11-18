e205241a54bddc7849ecaaf91fb4204f30e99015
R K <er.rupesh7@gmail.com>
2023-11-18 21:48:03 +0530

    mongodb connected with nextjs app and its little exp.

---
+++ src/app/api/db/route.js
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import { dbLink } from "@/app/db/db.config";
+import mongoose from "mongoose";
+
+const User = mongoose.models.User || mongoose.model("User", new mongoose.Schema({
+    username: String,
+    email: String,
+    pwd: String
+}));
+
+export async function GET() {
+  try {
+    await mongoose.connect(dbLink);
+
+    // Use the model to find all users
+    const dbData = await User.find();
+
+    console.log(dbData);
+
+    return NextResponse.json({ data: dbData });
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    return NextResponse.error({ error: "Internal Server Error" });
+  } finally {
+    // Always close the database connection, whether there's an error or not
+    mongoose.disconnect();
+    console.log('database disconnected !')
+  }
+}
---
+++ src/app/db/db.config.js
@@ -0,0 +1,3 @@
+let dbLink=`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PWD}@cluster0.sabb7q6.mongodb.net/experiment-next-js?retryWrites=true&w=majority`
+
+export  {dbLink}
